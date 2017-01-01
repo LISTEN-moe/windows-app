a632fa43114d191d6cc1b15b066ef36663e2d3ae
Sammy Sammon <anonymous-thing@hotmail.com>
2017-01-01 23:23:46 +1000

    Updated the program name/description to not "CrappyListenMoe"

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CrappyListenMoe")]
+[assembly: AssemblyTitle("Listen.moe Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CrappyListenMoe")]
+[assembly: AssemblyProduct("Listen.moe Client")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
